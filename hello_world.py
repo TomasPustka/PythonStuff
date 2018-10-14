b2ea63cf2aac542b23f3492067e2731ee1c156e0
TomasPustka <tomaspustka93@seznam.cz>
2018-10-15 00:05:21 +0200

    Some folium stuff

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,28 @@
-print("Hello World!")
-print("Why does not this work?")
+import folium
+import pandas
+
+def initialize_map():
+    return folium.Map(
+        location = [40,-100],
+        zoom_start = 5
+    )
+
+
+def load_volcanoes(path):
+    return pandas.read_csv(path)
+
+
+def place_marker_to_layer(marker, layer):
+    layer.add_child(marker)
+
+
+def iterate_through_data(data_volc):
+
+
+
+my_map = initialize_map()
+data_volc = load_volcanoes('Volcanoes_USA.txt')
+
+
+my_map.save('usa.html')
+
