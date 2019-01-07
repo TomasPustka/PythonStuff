5eebdd41741a1b8a773ca2e3637613cdac1265e7
TomasPustka <tomaspustka93@seznam.cz>
2019-01-07 13:13:41 +0100

    Learning selenium

--- api_tests.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-
-data = requests.get("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR").json()
-
-print(data['BTC'])
--- selenium_test.py
+++ selenium_test.py
@@ -1,20 +1,6 @@
 from selenium import webdriver
-import time
+
+driver = webdriver.Chrome()
+driver.get("www.google.com")
 
 
-def site_login(browser):
-    browser.find_element_by_id('os_username').send_keys('tomas.pustka@quest.com')
-    browser.find_element_by_id('os_password').send_keys('Warlord555')
-    browser.find_element_by_id('loginButton').click()
-
-
-
-
-browser = webdriver.Chrome()
-browser.get("https://wiki.labs.quest.com/display/TE/2018-10-16+Meeting+notes+-+Common+staff+meeting")
-
-site_login(browser)
-
-while True:
-    browser.refresh()
-    time.sleep(5)
